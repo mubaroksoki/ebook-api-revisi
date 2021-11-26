352f000087adcf11c907bb90e0824c695183fa6c
saukimubarok <sokigithub68@gmail.com>
2021-11-26 09:49:24 +0700

    membuat auth pake sanctum

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('users')->insert([
+            'name' => 'syauqi',
+            'email' => 'syauqi@mubarok.com',
+            'password' => Hash::make('1234')
+        ]);
+
+     
+        //
+    }
+}
